810cc10bae25d40e4bff644d845431c4c3529f7e
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2022-11-17 20:52:05 +0545

    adding update
    Signed-off-by: Prabesh Tiwari <prabeshtiwari1414@gmail.com>

--- main/Program.cs
+++ main/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hospital Management");
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public static void Main(String [] args){
+            int age;
+            Console.WriteLine("Hospital management");
+            Console.WriteLine("Click i for insert");
+            Console.WriteLine("Click u for update");
+            Console.WriteLine("Click d for delete");
+            string input = Console.ReadLine();
+            switch(input){
+                case "i": {
+                    Console.WriteLine("You click for insert");
+                    Console.WriteLine("Enter Patient name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter Patient Age");
+                    string agechar = Console.ReadLine();
+                    age=Convert.ToInt32(agechar);
+                    Console.WriteLine("Enter Patient Address");
+                    string address = Console.ReadLine();
+                    Console.WriteLine("Patient name is "+name+".  is "+age+" years old from "+address+".");
+                    break;
+                }
+                case "u": {
+                    Console.WriteLine("You click for update");
+                    break;
+                }
+                case "d": {
+                    Console.WriteLine("You click for delete");
+                    break;
+                }
+                
+            }
+        }   
+    }
+}
