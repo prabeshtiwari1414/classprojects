fac631ae361a265c4997e2d8c3589c1d321c6cee
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2022-11-18 16:01:11 +0545

    adding file
    Signed-off-by: Prabesh Tiwari <prabeshtiwari1414@gmail.com>

--- main/Program.cs
+++ main/Program.cs
@@ -3,24 +3,32 @@ namespace Name
 {
     class Program
     {
-        public static void Main(String [] args){
+        public static void Main(String [] args)
+        {
             int age;
-            Console.WriteLine("Hospital management");
-            Console.WriteLine("Click i for insert");
-            Console.WriteLine("Click u for update");
-            Console.WriteLine("Click d for delete");
+            Console.WriteLine("Hospital management \n Click i for insert \n Click u for update \n Click d for delete");
             string input = Console.ReadLine();
             switch(input){
                 case "i": {
-                    Console.WriteLine("You click for insert");
-                    Console.WriteLine("Enter Patient name");
+                    Console.WriteLine("You click for insert \n Enter Patient name");
                     string name = Console.ReadLine();
                     Console.WriteLine("Enter Patient Age");
                     string agechar = Console.ReadLine();
                     age=Convert.ToInt32(agechar);
                     Console.WriteLine("Enter Patient Address");
                     string address = Console.ReadLine();
-                    Console.WriteLine("Patient name is "+name+".  is "+age+" years old from "+address+".");
+                    Console.WriteLine("Patient name is "+name+".  is "+age+" years old from "+address+". \n Do you want to save it? 'y' for yes & n'for no(y/n)");
+                    string insert = Console.ReadLine();
+                    switch(insert){
+                        case "y": {
+                            Console.WriteLine("Sucessfully saved");
+                            break;
+                        }
+                        case "n": {
+                            Console.WriteLine("Data was nota saved");
+                            break;
+                        }
+                    }
                     break;
                 }
                 case "u": {
