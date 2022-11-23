c5ec63f073d56b88bc081bcc3fe442a4b395ded3
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2022-11-23 20:20:24 +0545

    changes
    Signed-off-by: Prabesh Tiwari <prabeshtiwari1414@gmail.com>

--- main/Program.cs
+++ main/Program.cs
@@ -17,6 +17,25 @@ namespace Name
                     age=Convert.ToInt32(agechar);
                     Console.WriteLine("Enter Patient Address");
                     string address = Console.ReadLine();
+                    Console.WriteLine("Enter gender {click m fo male , f for female and o for other }");
+                    string gender = Console.ReadLine();
+                    switch(gender){
+                        case "m": {
+                            Console.WriteLine("Patient name is "+name+". He is "+age+" years old from "+address+". \n Do you want to save it? 'y' for yes & 'n'for no(y/n)");
+
+                            break;
+                        }
+                        case "f": {
+                            Console.WriteLine("Patient name is "+name+". She is "+age+" years old from "+address+". \n Do you want to save it? 'y' for yes & 'n'for no(y/n)");
+
+                            break;
+                        }
+                        case "o": {
+                            Console.WriteLine("Patient name is "+name+" who is "+age+" years old from "+address+". \n Do you want to save it? 'y' for yes & 'n'for no(y/n)");
+
+                            break;
+                        }
+                    }
                     Console.WriteLine("Patient name is "+name+" who is "+age+" years old from "+address+". \n Do you want to save it? 'y' for yes & 'n'for no(y/n)");
                     string insert = Console.ReadLine();
                     switch(insert){
